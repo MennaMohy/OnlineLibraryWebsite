28b18674e522e42eb132750b9d8f7cdd9733f3b3
salma20005 <161861157+salma20005@users.noreply.github.com>
2025-04-19 13:55:34 +0200

    search

--- Books/Books.js
+++ Books/Books.js
@@ -6,7 +6,7 @@ const books =
             title: "A Tale of Two Cities",
             author: "Charles Dickens",
             category: "Classic Literature",
-            image: "tale_of_two_cities.jpg",
+            image: "Books/tale_of_two_cities.jpg",
             borrowed: false,
             description: "As Paris burns with the fury of the oppressed, the lives of an exiled aristocrat,\n" +
                 "        a selfless lawyer, and a woman caught between them collide in a storm of vengeance and redemption.\n" +
@@ -17,7 +17,7 @@ const books =
             title: "It Ends With Us",
             author: "Colleen Hoover",
             category: "Contemporary Romance",
-            image: "it_ends_with_us.jpg",
+            image: "Books/it_ends_with_us.jpg",
             borrowed: false,
             description: "  A powerful and emotional novel that follows Lily Bloom as she navigates love," +
             " heartbreak,and difficult choices. When her relationship with a charming neurosurgeon begins to crack," +
@@ -28,7 +28,7 @@ const books =
             title: "Beauty and the Beast",
             author: "Gabrielle-Suzanne Villeneuve",
             category: "Fairy Tale",
-            image: "beauty_and_the_beast.jpg",
+            image: "Books/beauty_and_the_beast.jpg",
             borrowed: false,
             description: " Beauty and the Beast is a timeless fairy tale that tells the story of a kind-hearted young " +
                 "woman named Beauty who sacrifices her freedom to save her father. She goes to live in the enchanted " +
@@ -40,7 +40,7 @@ const books =
             title: "Maybe Someday",
             author: "Colleen Hoover",
             category: "New Adult Romance",
-            image: "MaybeSomeday.jpg",
+            image: "Books/MaybeSomeday.jpg",
             borrowed: false,
             description: "When Sydney finds comfort in her neighbor Ridge's music, their creative spark turns\n" +
                 "    into something deeper. But some bonds come with consequences. Maybe Someday is a raw, emotional story about love\n" +
@@ -50,7 +50,7 @@ const books =
             title: "Practice Makes Perfect",
             author: "Sarah Adams",
             category: "Romantic Comedy",
-            image: "practice-makes-perfect.jpg",
+            image: "Books/practice-makes-perfect.jpg",
             borrowed: true,
             description: "Annie has always played it safe, until a fling with her brother's best friend\n" +
                 "        makes her question everything. As sparks fly, she discovers passion can't be planned—but is this just practice,\n" +
@@ -60,7 +60,7 @@ const books =
             title: "Things We Never Got Over",
             author: "Lucy Score",
             category: "Small-Town Romance",
-            image: "things-we-never-get-over.jpg",
+            image: "Books/things-we-never-get-over.jpg",
             borrowed: false,
             description: "When Naomi flees her wedding to rescue her estranged twin, she lands in Knockemout,\n" +
                 "    Virginia—and crashes straight into Knox, the brooding barber who hates drama. But with a niece to protect and a\n" +
@@ -70,7 +70,7 @@ const books =
             title: "Atomic Habits",
             author: "James Clear",
             category: "Self Development",
-            image: "atomic-habits.jpg",
+            image: "Books/atomic-habits.jpg",
             borrowed: true,
             description: "James Clear reveals how small, consistent habits compound into life-changing\n" +
                 "        outcomes. Packed with science-backed strategies, this book teaches you to build good habits, break bad ones,\n" +
@@ -80,7 +80,7 @@ const books =
             title: "How to Win Friends And Influence People",
             author: "Dale Carnegie",
             category: "Business",
-            image: "how-to-inf.jpg",
+            image: "Books/how-to-inf.jpg",
             borrowed: false,
             description: "Dale Carnegie's reveals the secrets to building relationships, winning\n" +
                 "        people over, and becoming a more effective communicator. Through real-life examples and practical principles,\n" +
@@ -90,7 +90,7 @@ const books =
             title: "The Antidote",
             author: "Karen Russell",
             category: "Dark Fiction",
-            image: "antidote.jpg",
+            image: "Books/antidote.jpg",
             borrowed: false,
             description: "When a desperate woman falls for a man whose touch is lethal, their twisted\n" +
                 "        bond blurs the line between passion and poison. As their dangerous game spirals, she must choose: succumb to\n" +
@@ -100,7 +100,7 @@ const books =
             title: "Fagin the Thief",
             author: "Allison Epstein",
             category: "Historical Fiction",
-            image: "fagain.jpg",
+            image: "Books/fagain.jpg",
             borrowed: false,
             description: "In this bold retelling of Dickens' infamous villain, Fagin emerges from the\n" +
                 "        shadows of Oliver Twist as a complex antihero navigating the brutal underworld of 19th-century London. Survival\n" +
@@ -110,7 +110,7 @@ const books =
             title: "Oliver Twist",
             author: "Charles Dickens",
             category: "Classic Literature",
-            image: "oliver.jpg",
+            image: "Books/oliver.jpg",
             borrowed: true,
             description: "Born in a workhouse where cruelty is the only lesson, young Oliver Twist\n" +
                 "        dares to ask for more. From Fagin's den of pickpockets to the terrifying grip of the violent Bill Sikes,\n" +
@@ -121,7 +121,7 @@ const books =
             title: "The Silent Patient",
             author: "Alex Michaelides",
             category: "Psychological Thriller",
-            image: "thesilentpatient.jpg",
+            image: "Books/thesilentpatient.jpg",
             borrowed: true,
             description: "Alicia seemed to have the perfect life until she shot her husband five times in the face and never spoke.\n" +
                 "        Confined to a psychiatric facility, her silence becomes a mystery that consumes criminal psychotherapist Theo\n" +
@@ -132,7 +132,7 @@ const books =
             title: "The Boy from the Woods",
             author: "Harlan Coben",
             category: "Mystery Thriller",
-            image: "theboyfromthewoods.jpg",
+            image: "Books/theboyfromthewoods.jpg",
             borrowed: false,
             description: "Wilde was found living wild in the woods as a child with no memory of his life.\n" +
                 "        Now an adult with military skills, he's drawn into danger when a teenage girl vanishes from their wealthy\n" +
@@ -143,7 +143,7 @@ const books =
             title: "The Summer of Broken Rules",
             author: "K.L. Walther",
             category: "Teen Romance",
-            image: "thesummerofbrokenrules.jpg",
+            image: "Books/thesummerofbrokenrules.jpg",
             borrowed: false,
             description: "Meredith escapes to her family's summer retreat, still grieving her sister's\n" +
                 "        death—until the annual game of Assassin throws her together with charming groomsman Wit. As sparks fly, she must\n" +
@@ -153,7 +153,7 @@ const books =
             title: "The Sun is Also a Star",
             author: "Nicola Yoon",
             category: "Contemporary Romance",
-            image: "thesunisalsoastar.jpeg",
+            image: "Books/thesunisalsoastar.jpeg",
             borrowed: false,
             description: "Practical Natasha is being deported tonight. Dreamy Daniel believes in fate.\n" +
                 "        When their paths collide in New York City, they have just 12 hours to prove love can exist between science and\n" +
@@ -163,7 +163,7 @@ const books =
             title: "Everything, Everything",
             author: "Nicola Yoon",
             category: "Contemporary Romance",
-            image: "everythingeverything.jpg",
+            image: "Books/everythingeverything.jpg",
             borrowed: false,
             description:"Madeline has spent her entire life sealed in a sterile home, allergic to the\n" +
                 "        outside world. When charismatic Olly moves in next door, his window-side antics spark a connection that makes\n" +
@@ -174,7 +174,7 @@ const books =
             title: "Confess",
             author: "Colleen Hoover",
             category: "New Adult Romance",
-            image: "confess.jpg",
+            image: "Books/confess.jpg",
             borrowed: false,
             description: "Auburn Reed needs $400 fast. When she walks into artist Owen Gentry's Dallas\n" +
                 "        studio, she finds more than a paycheck—she discovers anonymous confessions turned into breathtaking paintings,\n" +
@@ -185,7 +185,7 @@ const books =
             title: "Love Game",
             author: "Emma Rae",
             category: "Sports Romance",
-            image: "lovegame.jpg",
+            image: "Books/lovegame.jpg",
             borrowed: false,
             description:"Elle’s love language is food, but the culinary world hasn’t been kind—until she\n" +
                 "        lands her dream role as personal chef to tennis superstar Nicky Salco. Handsome, disciplined, and totally\n" +
@@ -196,7 +196,7 @@ const books =
             title: "Maybe in Another Life",
             author: "Taylor Jenkins Reid",
             category: "Contemporary Fiction",
-            image: "maybeinanotherlife.jpg",
+            image: "Books/maybeinanotherlife.jpg",
             borrowed: false,
             description: "Back in LA, Hannah's reunion with her first love splits her life into parallel\n" +
                 "        paths—showing how a single decision changes everything about love, loss, and finding yourself."
@@ -205,7 +205,7 @@ const books =
             title: "My Lovely Wife",
             author: "Samantha Downing",
             category: "Psychological Thriller",
-            image: "mylovelywife.jpg",
+            image: "Books/mylovelywife.jpg",
             borrowed: false,
             description: "A picture-perfect suburban couple hides a chilling secret - their marriage\n" +
                 "        thrives on murder. When their carefully crafted games escalate, the line between passion and predation blurs in\n" +
@@ -215,7 +215,7 @@ const books =
             title: "The Cheat Sheet",
             author: "Sarah Adams",
             category: "Contemporary Romance",
-            image: "thecheatsheet.jpg",
+            image: "Books/thecheatsheet.jpg",
             borrowed: false,
             description:"Pro football star Nathan Donelson has been secretly in love with his best friend,\n" +
                 "        dance studio owner Bree Camden, for years. When a viral video exposes his feelings, they strike a deal: pretend\n" +
@@ -226,7 +226,7 @@ const books =
             title: "The Enemy",
             author: "Sarah Adams",
             category: "Contemporary Romance",
-            image: "theenemy.jpg",
+            image: "Books/theenemy.jpg",
             borrowed: false,
             description: "Ambitious event planner Evie and laid-back musician Ryan can’t stand each\n" +
                 "        other—until a forced work collaboration leads to something else. Now, with their careers on the line and tension\n" +
@@ -236,7 +236,7 @@ const books =
             title: "Verity",
             author: "Colleen Hoover",
             category: "Psychological Thriller",
-            image: "Verity.jpg",
+            image: "Books/Verity.jpg",
             borrowed: false,
             description: "Struggling writer Lowen Ashleigh accepts a dream job: completing bestselling\n" +
                 "        author Verity Crawford's popular book series after a tragic accident leaves her unable to write. But when Lowen\n" +
@@ -247,7 +247,7 @@ const books =
             title: "When In Rome",
             author: "Sarah Adams",
             category: "Romantic Comedy",
-            image: "wheninrome.jpg",
+            image: "Books/wheninrome.jpg",
             borrowed: false,
             description: "When superstar Rae Rose ditches her tour for a midnight escape to Rome,\n" +
                 "        Kentucky, she crashes into Noah Walker’s bakery—and his life. He’s a pie shop owner who\n" +
@@ -258,7 +258,7 @@ const books =
             title: "The Housemaid",
             author: "Freida McFadden",
             category: "Psychological Thriller",
-            image: "Thehousemaid.jpg",
+            image: "Books/Thehousemaid.jpg",
             borrowed: false,
             description:"When Millie takes a job as a live-in maid for the wealthy Winchesters, she thinks\n" +
                 "        she's hit rock bottom. But behind their pristine facade lies something darker—a controlling wife, a haunted\n" +
