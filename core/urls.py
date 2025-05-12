656abbbaa3f815c57a1195dd2b5ae7536eab3969
nada <nadaamiin00@gmail.com>
2025-05-12 03:39:31 +0300

    linked signin and signup

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('signup/', views.signup_view, name='signup'),
+    path('signin/', views.signin_view, name='signin'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, 'WelcomePage.html')
+
+def signup_view(request):
+    return render(request, 'SignUp.html')
+
+def signin_view(request):
+    return render(request, 'SignIn.html')
